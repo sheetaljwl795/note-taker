c17bbc21ee94ac89a5f309fcdce227f246aefc6a
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-09-13 08:56:48 -0700

    render index.html and setup all notes.html

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('.', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+// GET request for ALL reviews
+app.get('/api/notes', (req, res) => {
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+  
+    // Sending all reviews to the client
+    return res.status(200).json(noteData);
+  });
+
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
