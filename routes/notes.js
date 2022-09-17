3129aad7ff63598326f76cd06c1c11d0cc74390e
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-09-16 21:21:44 -0700

    Updated server.js with index and notes.html file and added helper folder for uid, created routes for get and post API call

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding tip');
+    }
+  });
+  
+  module.exports = notes;
--- server.js
+++ server.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index');
+
 const PORT = process.env.port || 3004;
 
 const app = express();
-const noteData = require('./db/db.json');
-console.log(noteData)
 
-// Sets up the Express app to handle data parsing
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', api);
+
 
 app.use(express.static('public'));
 
-app.get('.', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-// GET request for ALL reviews
-app.get('/api/notes', (req, res) => {
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
-  
-    // Sending all reviews to the client
-    return res.status(200).json(noteData);
-  });
-
-
-
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
